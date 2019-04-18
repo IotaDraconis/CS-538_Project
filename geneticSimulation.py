0b383cea657e615c7edb7f9a9f0375ad8caf3fb2
IotaDraconis <paul.coen@siu.edu>
2019-04-18 18:41:10 -0500

    Updated changes

--- geneticSimulation.py
+++ geneticSimulation.py
@@ -11,74 +11,82 @@ import traci
 import traci.constants as tc
 
 from plexe import Plexe, ACC, CACC
-sys.path.append(os.path.abspath("/home/user/Documents/plexe-pyapi/examples"))
+sys.path.append(os.path.abspath("/home/pcoen/Documents/plexe-pyapi/examples"))
 from utils import add_platooning_vehicle
 
-from deap import base
-from deap import creator
-from deap import tools
+#from deap import base
+#from deap import creator
+#from deap import tools
 
-IND_SIZE=1
 LENGTH=4
-DISTANCE=5  # Possible tuing parameter for the GA
-SPEED = 25
+gui_mode = False
 
 
-def run_sumo_iteration(platoon_size):
-    traci.start(["sumo-gui", "-c", "sumocfg/freeway.sumo.cfg"])
+def remove(vehID, reason):
+    traci._sendByteCmd(tc.CMD_SET_VEHICLE_VARIABLE, tc.REMOVE, vehID, reason)
+
+def run_sumo_iteration(speed, platoon_size, distance):
+    if gui_mode == True:
+        traci.start(["sumo-gui", "-c", "cfg/freeway.sumo.cfg"])
+    else:
+        traci.start(["sumo", "-c", "cfg/freeway.sumo.cfg"])
+    
     plexe = Plexe()
     traci.addStepListener(plexe)
-    num_cars = 100
+    max_cars = 50
+    total_cars = 0
     total_fuel_used = 0
-    num_platoons = num_cars // platoon_size
+    num_platoons = max_cars // platoon_size
     src = None
     dest = None
     edge_list = traci.edge.getIDList()
-    edges = list()
+    edges_start = list()
+    edges_end = list()
+    
 
     for i in range(len(edge_list)):
         if(edge_list[i][:4] == "edge"):
-            edges.append(edge_list[i])
+            edges_start.append(edge_list[i])
+            edges_end.append(edge_list[i])
+            
 
     for i in range(num_platoons):
-        # Find a route for each platoon
-        route_found = False
-        #while(route_found == False):
-        #    src = random.choice(edges)
-        #    dest = random.choice(edges)
-        #    if(src != dest):
-        #        route_returned = traci.simulation.findRoute(fromEdge=src, toEdge=dest)
-        #        # print(route_returned.edges)
-        #        if(route_returned.edges != []):
-        #            route_found = True
-        #            # print("Route was found for a platoon!!!")
-        route_returned = traci.simulation.findRoute(fromEdge="edge_0_0", toEdge="edge_2_2")
+        route_returned = traci.simulation.findRoute(fromEdge="edge_0_0", toEdge="absorption_1")
         traci.route.add("route" + str(i), route_returned.edges)
-        
         LEADER = "car0_" + str(i)
-        
+
+        spawn_offset = (num_platoons - i + 1) * 2 * ((platoon_size + 1) * (distance + LENGTH))
+
         # Add platoon_size cars to a platoon
         for j in range(platoon_size):
+            total_cars += 1
             vid = "car" + str(j) + "_" + str(i)
-            add_platooning_vehicle(plexe, vid, 140 - j * (DISTANCE + LENGTH), 0, SPEED, DISTANCE, False)
-            plexe.set_fixed_lane(vid, 0, safe=False)
+            add_platooning_vehicle(plexe, vid, spawn_offset - j * (distance + LENGTH), 0, speed, distance, False, route="route" + str(i))
+            plexe.set_fixed_lane(vid, i % 4, safe=False)
             traci.vehicle.setSpeedMode(vid, 0)
-            if i == 0:
+            if j == 0:
                 plexe.set_active_controller(vid, ACC)
+                # Disable the ability to change lanes due to cars being dumb and changing lanes to get off on an off-ramps
                 plexe.enable_auto_lane_changing(LEADER, True)
+                #print("Creating leader of platoon: ", vid)
             else:
                 plexe.set_active_controller(vid, CACC)
                 plexe.enable_auto_feed(vid, True, LEADER, vid)
                 plexe.add_member(LEADER, vid, i)
+                #print("Adding member to platoon ", LEADER, " | ", vid)
+            if total_cars > max_cars:
+                break
 
+    print(total_cars)
 
-    #first_itteration = True
+    first_itteration = True
     #loop till done
     while traci.simulation.getMinExpectedNumber() > 0:
-        #if first_itteration == True:
-            #first_itteration == False
-            #traci.gui.trackVehicle("View #0", "car0_0")
-    
+        if first_itteration == True and gui_mode == True:
+             first_itteration == False
+             traci.gui.trackVehicle("View #0", "car0_0")
+             traci.gui.setZoom("View #0", 3000)
+
         for id in traci.vehicle.getIDList():
             total_fuel_used += traci.vehicle.getFuelConsumption(id)
         #        getFuelConsumption
@@ -101,15 +109,12 @@ def evaluate(individual):
     # Find individual fitness here
     return fit_value
 
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Individual", list, fitness=creator.FitnessMax)
-toolbox = base.Toolbox()
-toolbox.register("attr_float", random.random)
-toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_float, n=IND_SIZE)
 
 
 
-run_sumo_iteration(1);
+
+
+run_sumo_iteration(100, 25, 1);
 
 
 
